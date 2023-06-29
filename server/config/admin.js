b3967c9f0194caae9af55c54c6b377df6c6f064e
Mohammed Dwina <mohammeddwina@Mohammeds-MacBook-Pro.local>
2023-06-29 20:57:28 +0100

    fix auth issue

--- server/config/admin.js
+++ server/config/admin.js
@@ -1,8 +1,10 @@
+const crypto = require('crypto');
+
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', crypto.randomBytes(16).toString('base64')),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', crypto.randomBytes(16).toString('base64')),
   },
 });
