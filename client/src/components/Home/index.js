1c9929cb25afb2e6e5604355e8b754482e42c124
Mohammed Dwina <diwina1993@hotmail.com>
2023-07-01 17:50:33 +0100

    Update products rendering

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,36 +1,36 @@
 import React from "react";
 import Product from "./Product";
+import CustomNav from "../CustomNav";
 import { Row, Col } from "reactstrap";
 import { useProducts } from "./useProducts";
-import { Link } from "react-router-dom";
 
 const Home = () => {
   const { categories, products } = useProducts();
 
   return (
     <div>
+      <CustomNav />
       <div className="home">
         <h2 style={{ textAlign: "center" }}>Enjoy our sales!</h2>
-        {categories.length &&
-          categories.map((category) => {
-            const hasProducts = products.filter(
-              (product) => product.attributes.category.data.id === category.id
-            );
-            return hasProducts && hasProducts.length ? (
-              <>
-                <h2 className="category-title">{category.attributes.name}</h2>
-                <Row key={category.id} className="category">
-                  {hasProducts.map((product) => (
-                    <Col sm="12" md="4" key={product.id}>
-                      <Link to={`/product-details/${product.id}`}>
+        {categories.length
+          ? categories.map((category) => {
+              const hasProducts = products.filter(
+                (product) => product.attributes.category.data.id === category.id
+              );
+              return hasProducts && hasProducts.length ? (
+                <>
+                  <h2 className="category-title">{category.attributes.name}</h2>
+                  <Row key={category.id} className="category">
+                    {hasProducts.map((product) => (
+                      <Col sm="12" md="4" key={product.id}>
                         <Product product={product} />
-                      </Link>
-                    </Col>
-                  ))}
-                </Row>
-              </>
-            ) : null;
-          })}
+                      </Col>
+                    ))}
+                  </Row>
+                </>
+              ) : null;
+            })
+          : null}
       </div>
     </div>
   );
