9533387f1b3f14536e267c3767c0940a07de9384
Mohammed Dwina <diwina1993@hotmail.com>
2023-08-18 10:39:57 +0100

    Remove new HomePage

--- server/src/admin/app.js
+++ server/src/admin/app.js
@@ -17,52 +17,21 @@ const config = {
   translations: {
     en: {
       "app.components.LeftMenu.navbrand.title": "Store Dashboard",
+
       "app.components.LeftMenu.navbrand.workplace": "Testing",
+
       "Auth.form.welcome.title": "Welcom to Dwinatech Store",
+
       "Auth.form.welcome.subtitle": "Login to your account",
+
       "Settings.profile.form.section.experience.interfaceLanguageHelp":
         "Preference changes will apply only to you.",
-      "Admin.pages.homepage.title": "Welcome To Dwinatech Store",
-    },
-    ar: {
-      "app.components.LeftMenu.navbrand.title": "لوحة المخذن",
-      "Settings.profile.form.section.experience.interfaceLanguageHelp":
-        "Preference changes will apply only to you.",
-      "Admin.pages.homepage.title": "مرحبا بك في متجر Dwinatech",
     },
   },
   // Disable video tutorials
   tutorials: false,
   // Disable notifications about new Strapi releases
   notifications: { releases: false },
-  locales: [
-    "ar",
-    // 'fr',
-    // 'cs',
-    // 'de',
-    // 'dk',
-    // 'es',
-    // 'he',
-    // 'id',
-    // 'it',
-    // 'ja',
-    // 'ko',
-    // 'ms',
-    // 'nl',
-    // 'no',
-    // 'pl',
-    // 'pt-BR',
-    // 'pt',
-    // 'ru',
-    // 'sk',
-    // 'sv',
-    // 'th',
-    // 'tr',
-    // 'uk',
-    // 'vi',
-    // 'zh-Hans',
-    // 'zh',
-  ],
 };
 
 const bootstrap = (app) => {
