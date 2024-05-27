ebe1e6353099640250d9ed99eacedb6b566ab579
Mohammed Dwina <diwina1993@hotmail.com>
2024-05-27 20:34:48 +0100

    Update Port number

--- clientRunner.js
+++ clientRunner.js
@@ -9,7 +9,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
